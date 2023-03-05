cbb3395bdf54ba3a0e1c04c2940aaf220d541fb4
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2023-03-05 23:16:42 +0300

    created users

--- db/migrate/20230227130750_user_details.rb
+++
@@ -1,7 +0,0 @@
-class UserDetails < ActiveRecord::Migration[6.1]
-  def change
-    create_table :user_details do |t|
-      t.string :students
-    end
-  end
-end
---
+++ db/migrate/20230305201310_create_skills.rb
@@ -0,0 +1,4 @@
+class CreateSkills < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
